71d15b8afad452512fc5ba920ddcfddec01e52b7
TimurHay <108826959+TimurHay@users.noreply.github.com>
2022-07-27 18:09:58 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$('a[href^="#"').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+});
+
+
+
+
+
